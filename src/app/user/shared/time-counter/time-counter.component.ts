31c08e992ac5a044580ea7c644d60c013a442d3d
Fedi Ghourabi <fedisaid.ghourabi@esprit.tn>
2023-06-05 20:17:52 +0100

    Added a countdown counter for reservation time limit

---
+++ src/app/user/shared/time-counter/time-counter.component.ts
@@ -0,0 +1,89 @@
+import {Component, OnDestroy} from '@angular/core';
+import {interval, Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-time-counter',
+  templateUrl: './time-counter.component.html',
+  styleUrls: ['./time-counter.component.css']
+})
+export class TimeCounterComponent implements OnDestroy {
+  timeRemaining: any = '';
+
+
+  private counterSubscription?: Subscription;
+  hours: string = '00';
+  minutes: string = '00';
+  seconds: string = '00';
+  isExpired: boolean = false;
+
+
+  constructor() {
+    this.startCounter();
+  }
+
+  ngOnDestroy() {
+    this.stopCounter();
+  }
+
+  ngOnInit() {
+    this.startCountdownAt18();
+  }
+
+  private startCountdownAt18() {
+    const currentDate = new Date();
+    const currentDayOfWeek = currentDate.getDay();
+    const currentHour = currentDate.getHours();
+
+    if (currentDayOfWeek >= 1 && currentDayOfWeek <= 5 && currentHour < 18) {
+      const timeUntil18 = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 18, 0, 0).getTime() - currentDate.getTime();
+      setTimeout(() => {
+        this.startCounter();
+      }, timeUntil18);
+    } else {
+      this.startCounter();
+    }
+  }
+
+  private startCounter() {
+    this.counterSubscription = interval(1000).subscribe(() => {
+      const currentDate = new Date();
+      const currentHour = currentDate.getHours();
+      const currentDay = currentDate.getDate();
+      const next6pm = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDay + 1 , 18, 0, 0);
+      const next10am = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDay + 1 , 10, 0, 0);
+
+      if (currentHour >= 18) {
+        const timeDiff = next10am.getTime() - currentDate.getTime();
+        const remainingTime = timeDiff > 0 ? timeDiff : 0;
+
+        this.timeRemaining = this.formatRemainingTime(remainingTime);
+      } else if (currentHour >= 0 && currentHour < 18) {
+        const timeDiff = next6pm.getTime() - currentDate.getTime();
+        const remainingTime = timeDiff > 0 ? timeDiff : 0;
+
+        this.timeRemaining = this.formatRemainingTime(remainingTime);
+      } else {
+        this.timeRemaining = '';
+      }
+    });
+  }
+
+  private formatRemainingTime(timeDiff: number) {
+    const hours = Math.floor(timeDiff / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const minutes = Math.floor((timeDiff / (1000 * 60)) % 60).toString().padStart(2, '0');
+    const seconds = Math.floor((timeDiff / 1000) % 60).toString().padStart(2, '0');
+
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+
+    this.isExpired = timeDiff <= 0;
+
+  }
+
+  private stopCounter() {
+    if (this.counterSubscription) {
+      this.counterSubscription.unsubscribe();
+    }
+  }
+}
